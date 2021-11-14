be7d84f517bfe30195b8cd8b6754c655d6abba48
Asif-Ul-Karim-Nabir <karimnabir17@gmail.com>
2021-11-14 12:17:52 +0600

    third-commit

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function run () {
     })
 
     // Delete api
-    app.delete('orders/:id',async(req,res)=>{
+    app.get('orders/:id',async(req,res)=>{
       const id = req.params.id;
       const query = {_id : ObjectId(id)}
       const orderDelete = await orderCollections.deleteOne(query)
